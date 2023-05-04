9bea3f283d4f68452105f353ab2e09057f6a229b
yassineaitalla <123477352+yassineaitalla@users.noreply.github.com>
2023-05-04 19:04:05 +0200

    Delete valider_commande.php

--- valider_commande.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'include/database.php';
-$id = $_GET['id'];
-$etat = $_GET['etat'];
-$sqlState = $pdo->prepare('UPDATE commande SET valide = ? WHERE id = ?');
-$sqlState->execute([$etat, $id]);
-header('location: commande.php?id=' . $id);
