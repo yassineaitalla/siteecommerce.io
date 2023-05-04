64982ff16a183b9236a38f7fc6ba1179b16ef2ba
yassineaitalla <123477352+yassineaitalla@users.noreply.github.com>
2023-05-04 19:05:38 +0200

    Delete supprimer_categorie.php

--- supprimer_categorie.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once 'include/database.php';
-    $id = $_GET['id'];
-    $sqlState = $pdo->prepare('DELETE FROM categorie WHERE id=?');
-    $supprime = $sqlState->execute([$id]);
-    header('location: categories.php');
